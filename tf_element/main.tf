ea3d891db7c1cf210247e5e440f9f31f077d26b5
stephaniedangelo <sdangelo.s@hotmail.com>
2022-04-04 19:04:38 -0300

    Create main.tf

---
+++ tf_element/main.tf
@@ -0,0 +1,12 @@
+variable "planos" {
+	type = map
+	default = {
+		"small" = "1xCPU-1GB"
+		"medium" = "1xCPU-2GB"
+		"large" = "2xCPU-4GB"
+	}
+}
+
+output "valores" {
+	value = value(var.planos)
+}
